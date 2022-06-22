812ce39bf9aea2d88a2914ce0c83cffd76f9bb2a
lakshmin657 <lakshmin657@gmail.com>
2022-06-22 15:34:26 +0530

    uptdated

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 iam updating the code master
-=======
 here iam upadeting code devolopment
->>>>>>> devolopment
 im updated this logic
-
 hello im updating this code development branch file
 
 This file contails deployment script
